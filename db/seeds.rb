3fa5e6151b39a4c7e3a5416b251a985ce3ed30a1
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-02-12 14:31:11 +0800

    delete additional error document

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-u = User.new
-u.email = "zhengzhi2616@gmail.com"           # 可以改成自己的 email
-
-u.password = "Zz2936821"                # 最少要六码
-
-u.password_confirmation = "Zz2936821"   # 最少要六码
-
-u.is_admin = true
-u.save
