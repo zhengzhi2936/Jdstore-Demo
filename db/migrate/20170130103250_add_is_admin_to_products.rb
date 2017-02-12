36c08df042e55597337000b4990b0ba420f6d27b
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-02-12 14:59:04 +0800

    db & log not clean

--- db/migrate/20170130103250_add_is_admin_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddIsAdminToProducts < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :is_admin, :boolean, default: false
-  end
-end
--- db/migrate/20170203052352_add_nickname_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNicknameToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :nickname, :string
-  end
-end
