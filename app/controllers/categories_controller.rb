3397a07c279757fc9a57c0efb0ab689d68a8e89c
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-02-13 16:42:46 +0800

    add user.name to navbar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :require_is_admin
 
   # GET /categories
   # GET /categories.json
