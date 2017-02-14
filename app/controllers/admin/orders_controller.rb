b9b79976211e4aaebd310ae14e94f7dbb4766675
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-02-14 11:10:21 +0800

    order completed

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrdersController < ApplicationController
   layout "admin"
 
-    before_action :authenticate_user
+    before_action :authenticate_user!
     before_action :require_is_admin
 
   def index
