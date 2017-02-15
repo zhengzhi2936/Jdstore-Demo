d138c215bcba162eda95503202a72b35da3d2b65
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-02-15 08:53:11 +0800

    add development host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-
+config.action_mailer.default_url_options = { :host => "small-shop.herokuapp.com" }
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
