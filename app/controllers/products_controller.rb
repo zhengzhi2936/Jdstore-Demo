1edfb851b8e1ba4ddb43f63f15d62879b83c500f
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-02-15 14:50:34 +0800

    before push to heroku

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,9 +24,9 @@ class ProductsController < ApplicationController
     @posts = @product.posts
     @prints = @product.prints.all
     if @product.posts.present?
-    @post = Post.find(params[:id])
+    @post = @product.posts.find_by(params[:id])
     @graphics = @post.graphics.all
-    end
+  end 
   end
   def search
     if @query_string.present?
