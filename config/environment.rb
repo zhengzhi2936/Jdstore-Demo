2758b4c870492de928d3eb9a8a40a2d1fbc3f5e2
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-02-15 08:26:41 +0800

    add host in order to fix heroku bugs

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
-
+config.action_mailer.default_url_options = { :host => 'small-shop.herokuapp.com' }
 # Initialize the Rails application.
 Rails.application.initialize!
