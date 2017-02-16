b5314126c71c4ddaef960a420c909d708c08b7e0
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-02-16 08:58:06 +0800

    improve image quality

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -21,3 +21,14 @@ CarrierWave.configure do |config|
     config.storage :file
   end
 end
+module CarrierWave
+  module MiniMagick
+    def quality(percentage)
+      manipulate! do |img|
+        img.quality(percentage)
+        img = yield(img) if block_given?
+        img
+      end
+    end
+  end
+end
