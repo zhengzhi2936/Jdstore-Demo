68c337d19dd388db6327df1623545c8f9888920f
raimonfuns <raimonfuns@163.com>
2017-02-19 22:08:59 +0800

    fixed comment width bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,27 +38,6 @@ $(document).on('click', '.backtop', function () {
 	})
 })
 
-// 预览图选择
-$(document).on('mouseover', '.intro-preview-item', function () {
-	var src = $(this).find('img').attr('src')
-	$('.intro-bigPic img').attr('src', src)
-    $('#thumb-lens').css('background-image', 'url(' + src + ')')
-	$(this).addClass('intro-preview-activeItem').siblings().removeClass('intro-preview-activeItem')
-	m.attach({
-        thumb: '#thumb',
-        large: src,
-        largeWrapper: 'preview'
-	})
-})
-
-$('.intro-preview-activeItem').trigger('mouseover')
-$(document).on('mouseover', '.magnifier-thumb-wrapper', function (e) {
-    $('#preview').css('visibility', 'visible')
-})
-$(document).on('mouseout', '.magnifier-thumb-wrapper', function (e) {
-    $('#preview').css('visibility', 'hidden')
-})
-
 // 拉票小功能
 var $header = $('.header')
 $(window).scroll(function () {
@@ -107,4 +86,25 @@ m.attach({
     large: $('.intro-preview-activeItem img').attr('src'),
     largeWrapper: 'preview',
     zoom: 2
+})
+
+// 预览图选择
+$(document).on('mouseover', '.intro-preview-item', function () {
+	var src = $(this).find('img').attr('src')
+	$('.intro-bigPic img').attr('src', src)
+    $('#thumb-lens').css('background-image', 'url(' + src + ')')
+	$(this).addClass('intro-preview-activeItem').siblings().removeClass('intro-preview-activeItem')
+	m.attach({
+        thumb: '#thumb',
+        large: src,
+        largeWrapper: 'preview'
+	})
+})
+
+$('.intro-preview-activeItem').trigger('mouseover')
+$(document).on('mouseover', '.magnifier-thumb-wrapper', function (e) {
+    $('#preview').css('visibility', 'visible')
+})
+$(document).on('mouseout', '.magnifier-thumb-wrapper', function (e) {
+    $('#preview').css('visibility', 'hidden')
 })
