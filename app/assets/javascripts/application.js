d6cbeb0ef219a1716723ba7973a28dd731fd46d0
raimonfuns <raimonfuns@163.com>
2017-02-20 23:13:02 +0800

    add cart-dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,13 +30,13 @@ $(document).ready(function() {
 
 // 点击事件
 $(document).on('click', '.backtop', function () {
-	$('body').animate({'scrollTop': 0}, 500)
+  $('body').animate({'scrollTop': 0}, 500)
 }).on('mouseenter', '.support', function () {
-	$('.ewm').show().stop().animate({left: '-136px', opacity: 1}, 500)
+  $('.ewm').show().stop().animate({left: '-136px', opacity: 1}, 500)
 }).on('mouseleave', '.support', function () {
-	$('.ewm').stop().animate({opacity: 0}, 500, function () {
-		$(this).css('left', 0).hide()
-	})
+  $('.ewm').stop().animate({opacity: 0}, 500, function () {
+    $(this).css('left', 0).hide()
+  })
 })
 
 // 拉票小功能
@@ -45,32 +45,32 @@ var $sidebar = $('#sidebar')
 var $category_sidebar = $('.category_sidebar')
 
 $(window).scroll(function () {
-	if ($(this).scrollTop() > 500) {
-		$sidebar.fadeIn()
-		$category_sidebar.fadeIn()
-	} else {
-		$sidebar.fadeOut()
-		$category_sidebar.fadeOut()
-	}
+  if ($(this).scrollTop() > 500) {
+    $sidebar.fadeIn()
+    $category_sidebar.fadeIn()
+  } else {
+    $sidebar.fadeOut()
+    $category_sidebar.fadeOut()
+  }
 
-	if ($(this).scrollTop() > 300) {
-		if ($header.is(':animated')) {
-			return false
-		}
-		$header.addClass('header_fixed')
-		$header.stop().animate({top: 0})
-		$('.header_placeholder').show()
-	} else {
-		$header.css({top: -80})
-		$header.removeClass('header_fixed')
-		$('.header_placeholder').hide()
-	}
+  if ($(this).scrollTop() > 300) {
+    if ($header.is(':animated')) {
+      return false
+    }
+    $header.addClass('header_fixed')
+    $header.stop().animate({top: 0})
+    $('.header_placeholder').show()
+  } else {
+    $header.css({top: -80})
+    $header.removeClass('header_fixed')
+    $('.header_placeholder').hide()
+  }
 })
 
 // 详情菜单栏切换
 $(document).on('click', '.productDetail-tabList-tab', function () {
-	$(this).addClass('productDetail-tabList-activeTab').siblings().removeClass('productDetail-tabList-activeTab')
-	$('.productDetail-content').eq($(this).index()).show().siblings().hide()
+  $(this).addClass('productDetail-tabList-activeTab').siblings().removeClass('productDetail-tabList-activeTab')
+  $('.productDetail-content').eq($(this).index()).show().siblings().hide()
 })
 
 
@@ -83,34 +83,49 @@ $('.comment-image').click(function () {
     }
 })
 
+// 投票
 $('.star-look').raty({
-      path: '/ratyrate/',
-          readOnly: true,
-          score: function() {
-        return $(this).attr('data-score');
-      }
-});$('.star-price').raty({
-      path: '/ratyrate/',
-          readOnly: true,
-          score: function() {
-        return $(this).attr('data-score');
-      }
+  path: '/ratyrate/',
+    readOnly: true,
+    score: function() {
+    return $(this).attr('data-score');
+  }
 });
 
-    $('#star-rating').raty({
-    path: '/ratyrate/',
-    scoreName: 'post[rating]'
+$('.star-price').raty({
+  path: '/ratyrate/',
+    readOnly: true,
+    score: function() {
+    return $(this).attr('data-score');
+  }
+});
+
+$('.star-rating').raty({
+  path: '/ratyrate/',
+    readOnly: true,
+    score: function() {
+    return $(this).attr('data-score');
+  }
+});
+
+$('#star-rating').raty({
+  path: '/ratyrate/',
+  scoreName: 'post[rating]'
 });
 
 $('#star-look').raty({
-path: '/ratyrate/',
-scoreName: 'post[look]'
+  path: '/ratyrate/',
+  scoreName: 'post[look]'
 });
 
 $('#star-price').raty({
-    path: '/ratyrate/',
-    scoreName: 'post[price]'
+  path: '/ratyrate/',
+  scoreName: 'post[price]'
 });
+
+$('#star-rating img').eq(0).trigger('click')
+$('#star-look img').eq(0).trigger('click')
+$('#star-price img').eq(0).trigger('click')
 
 // 放大镜
 $('#preview').css('visibility', 'hidden')
@@ -125,15 +140,15 @@ m.attach({
 
 // 预览图选择
 $(document).on('mouseover', '.intro-preview-item', function () {
-	var src = $(this).find('img').attr('src')
-	$('.intro-bigPic img').attr('src', src)
+  var src = $(this).find('img').attr('src')
+  $('.intro-bigPic img').attr('src', src)
     $('#thumb-lens').css('background-image', 'url(' + src + ')')
-	$(this).addClass('intro-preview-activeItem').siblings().removeClass('intro-preview-activeItem')
-	m.attach({
+  $(this).addClass('intro-preview-activeItem').siblings().removeClass('intro-preview-activeItem')
+  m.attach({
         thumb: '#thumb',
         large: src,
         largeWrapper: 'preview'
-	})
+  })
 })
 
 $('.intro-preview-activeItem').trigger('mouseover')
@@ -143,3 +158,5 @@ $(document).on('mouseover', '.magnifier-thumb-wrapper', function (e) {
 $(document).on('mouseout', '.magnifier-thumb-wrapper', function (e) {
     $('#preview').css('visibility', 'hidden')
 })
+
+
