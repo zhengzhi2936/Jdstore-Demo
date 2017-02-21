41419b35101f8c675d5cff1d72dd5c5b7b3dc8f2
raimonfuns <raimonfuns@163.com>
2017-02-21 14:28:23 +0800

    update order send method

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,8 +4,7 @@ class OrderMailer < ApplicationMailer
       @user        = order.user
       @product_lists = @order.product_lists
 
-      mail to: @user.email, subject: "Success! You did it."
-      # mail(to: @user.email , subject: "[JDstore] 感谢您完成本次的下单，以下是您这次购物明细 #{order.token}")
+      mail(to: @user.email , subject: "[JDstore] 感谢您完成本次的下单，以下是您这次购物明细 #{order.token}")
   end
   def apply_cancel(order)
       @order       = order
