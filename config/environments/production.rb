33a9552812ff41777ea5bf4887ad7daa1b2f9c7c
raimonfuns <raimonfuns@163.com>
2017-02-21 13:41:40 +0800

    update mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,26 +83,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'small-shop.herokuapp.com'}
-
-  # config.action_mailer.delivery_method = :smtp
-  # ActionMailer::Base.smtp_settings = {
-  #   address: "smtpcloud.sohu.com",
-  #   port: 25,
-  #   domain: "small-shop.herokuapp.com",
-  #   authentication: "plain",
-  #   enable_starttls_auto: true,
-  #   user_name: ENV["SEND_CLOUD_USER_NAME"],
-  #   password: ENV["SEND_CLOUD_USER_KEY"]
-  # }
+  config.action_mailer.default_url_options = { :host => 'http://small-shop.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'small-shop.herokuapp.com', #eg: 'yourappname.herokuapp.com'
-    :authentication => :plain,
- }
+  ActionMailer::Base.smtp_settings = {
+    address: "smtpcloud.sohu.com",
+    port: 25,
+    domain: "small-shop.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["SEND_CLOUD_USER_NAME"],
+    password: ENV["SEND_CLOUD_USER_KEY"]
+  }
 end
