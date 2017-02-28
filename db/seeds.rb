8fda01857e9e2c0935c4611b564aae9b9f25dd65
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-02-28 23:39:08 +0800

    change admin password

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 u = User.new
 u.email = "zhengzhi2616@gmail.com"           # 可以改成自己的 email
 
-u.password = "Zz2936821"                # 最少要六码
+u.password = "abc123123"                # 最少要六码
 
-u.password_confirmation = "Zz2936821"   # 最少要六码
+u.password_confirmation = "abc123123"   # 最少要六码
 
 u.is_admin = true
 u.save
